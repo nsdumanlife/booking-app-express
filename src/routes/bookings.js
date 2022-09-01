49087a435e2ae2e2dc24c91bc2baeef5e3434f3a
NSDuman <dumanlife@yandex.com>
2022-09-01 16:15:45 +0300

    Create bookings template and edit it's route file

--- src/routes/bookings.js
+++ src/routes/bookings.js
@@ -1,11 +1,12 @@
 const express = require('express')
-const flatted = require('flatted')
 const signUp = require('../models/sign-up')
 const Bungalow = require('../models/bungalow')
 
 const router = express.Router()
 
-const dogancay = new Bungalow('Dogancay', 'Geyve, Sakarya, Turkey', 5, 1250)
+const dogancay = new Bungalow('Dogancay', 'Geyve, Sakarya, Turkey', 5, 1250, 'Faruk')
+const tepe = new Bungalow('Tepe', 'Sapanca, Sakarya', 8, 950, 'Faruk')
+const lion = new Bungalow('Lion', 'Kumbag, Tekirdag', 4, 1250, 'Faruk')
 const numan = signUp('Numan', 'Duman', 'nsduman@gmail.com', 29)
 
 const checkInDate = new Date('09/22/2022')
@@ -16,12 +17,13 @@ const checkInDate2 = new Date('01/29/2023')
 const checkOutDate2 = new Date('02/02/2023')
 
 numan.book(dogancay, checkInDate, checkOutDate)
-numan.book(dogancay, checkInDate1, checkOutDate1)
-numan.book(dogancay, checkInDate2, checkOutDate2)
+numan.book(tepe, checkInDate1, checkOutDate1)
+numan.book(lion, checkInDate2, checkOutDate2)
 
 /* GET bookings listing. */
 router.get('/', (req, res) => {
-	res.send(flatted.toJSON(numan.bookings))
+	// res.send(flatted.toJSON(numan.bookings))
+	res.render('bookings', { title: 'Bookings', user: numan })
 })
 
 module.exports = router
