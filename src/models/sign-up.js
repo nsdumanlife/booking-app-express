76af152836d9ac6315892635b32b6e93dc1c1c07
NSDuman <dumanlife@yandex.com>
2022-09-03 23:13:17 +0300

    Create sample instance and export

--- src/models/sign-up.js
+++ src/models/sign-up.js
@@ -1,6 +1,9 @@
 const User = require('./user')
 
-module.exports = function signUp(firstName, lastName, email, age) {
+function signUp(firstName, lastName, email, age) {
 	// TODO: check that email address is available or not
 	return new User(firstName, lastName, email, age)
 }
+const numan = signUp('Numan', 'Duman', 'nsduman@gmail.com', 29)
+
+module.exports = { signUp, numan }
