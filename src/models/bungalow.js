dc1de5b0a785843a1edbd39fcff3ca2712cd3a60
NSDuman <dumanlife@yandex.com>
2022-09-03 21:49:49 +0300

    Add images to sample instance's images property

--- src/models/bungalow.js
+++ src/models/bungalow.js
@@ -1,5 +1,6 @@
 const { v4: uuidv4 } = require('uuid')
 const getDays = require('./get-booking-days')
+const { imagesOfOxygen } = require('./image')
 
 class Bungalow {
 	constructor(name, location, capacity, price, owner) {
@@ -13,7 +14,7 @@ class Bungalow {
 		this.price = price
 		this.reviews = [] // user's reviews
 		this.rates = [] // average of stars are given by users
-		this.photos = [] //
+		this.images = [] //
 		this.services = [] // internet, barbecue,hot tub, pool, hot water, kitchen etc.
 
 		this.owner = owner
@@ -34,6 +35,7 @@ const tepe = new Bungalow('Tepe', 'Sapanca, Sakarya', 8, 950, 'Faruk')
 const lion = new Bungalow('Lion', 'Kumbag, Tekirdag', 4, 1250, 'Faruk')
 const dogancay = new Bungalow('Dogancay', 'Geyve, Sakarya, Turkey', 5, 1150, 'Faruk')
 const oxygen = new Bungalow('Oxygen', 'Geyve, Sakarya', 8, 950, 'Numan')
+oxygen.images.push(...imagesOfOxygen)
 const bungalows = [tepe, lion, dogancay, oxygen]
 
 module.exports = { Bungalow, bungalows }
