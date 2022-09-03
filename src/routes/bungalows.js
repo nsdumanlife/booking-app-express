b42df327ee64dec93b0728ffe8c4f4384c72c911
NSDuman <dumanlife@yandex.com>
2022-09-03 23:16:00 +0300

    Import user instance and pass it to view

--- src/routes/bungalows.js
+++ src/routes/bungalows.js
@@ -1,6 +1,7 @@
 const express = require('express')
 // const flatted = require('flatted')
 const { bungalows } = require('../models/bungalow')
+const { numan } = require('../models/sign-up')
 
 const router = express.Router()
 
@@ -9,14 +10,14 @@ router.get('/', (req, res) => {
 	if (req.query.name) {
 		return res.send(bungalows.filter(bungalow => bungalow.name.toLowerCase() === req.query.name.toLowerCase()))
 	}
-	return res.render('bungalows', { title: `Rent a Bungalow for Your Next Escape`, bungalows })
+	return res.render('bungalows', { title: `Rent a Bungalow for Your Next Escape`, bungalows, user: numan })
 })
 
 router.get('/:bungalowId', (req, res) => {
 	const selectedBungalow = bungalows.find(bungalow => bungalow.id === req.params.bungalowId)
 
 	// if (bungalow) res.send(flatted.toJSON(bungalow))
-	if (selectedBungalow) res.render('bungalow', { title: `${selectedBungalow.name}`, selectedBungalow })
+	if (selectedBungalow) res.render('bungalow', { title: `${selectedBungalow.name}`, bungalow: selectedBungalow })
 	else res.sendStatus(404)
 })
 
