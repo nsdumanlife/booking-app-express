85f6bae42b1b08e6a6db5629fc4ab76de644b545
NSDuman <dumanlife@yandex.com>
2022-09-05 17:46:08 +0300

    Edit user to logged in user

--- src/routes/bookings.js
+++ src/routes/bookings.js
@@ -1,12 +1,13 @@
 const express = require('express')
-const { numan } = require('../models')
+const { users, loggedInUser } = require('../models')
 
 const router = express.Router()
 
 /* GET bookings listing. */
 router.get('/', (req, res) => {
+	const user = users.find(u => u.name === loggedInUser.name)
 	// res.send(flatted.toJSON(numan.bookings))
-	res.render('bookings', { title: 'Bookings', user: numan })
+	res.render('bookings', { title: 'Bookings', user: loggedInUser, loggedIn: user === loggedInUser })
 })
 
 module.exports = router
