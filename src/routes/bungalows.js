867f2916e3481ae13d37ebc2bff00707d8173708
NSDuman <dumanlife@yandex.com>
2022-09-05 17:47:36 +0300

    Create add review post request and form

--- src/routes/bungalows.js
+++ src/routes/bungalows.js
@@ -1,7 +1,7 @@
 const express = require('express')
 // const flatted = require('flatted')
 const { bungalows } = require('../models')
-const { numan } = require('../models')
+const { loggedInUser } = require('../models')
 
 const router = express.Router()
 
@@ -10,15 +10,45 @@ router.get('/', (req, res) => {
 	if (req.query.name) {
 		return res.send(bungalows.filter(bungalow => bungalow.name.toLowerCase() === req.query.name.toLowerCase()))
 	}
-	return res.render('bungalows', { title: `Rent a Bungalow for Your Next Escape`, bungalows, user: numan })
+	return res.render('bungalows', { title: `Rent a Bungalow for Your Next Escape`, bungalows, user: loggedInUser })
 })
 
 router.get('/:bungalowId', (req, res) => {
 	const selectedBungalow = bungalows.find(bungalow => bungalow.id === req.params.bungalowId)
 
 	// if (bungalow) res.send(flatted.toJSON(bungalow))
-	if (selectedBungalow) res.render('bungalow', { title: `${selectedBungalow.name}`, bungalow: selectedBungalow })
+	if (selectedBungalow)
+		res.render('bungalow', { title: `Bungalow ${selectedBungalow.name}`, bungalow: selectedBungalow })
 	else res.sendStatus(404)
+})
+
+router.post('/:bungalowId/bookings', (req, res) => {
+	const bungalow = bungalows.find(bung => bung.id === req.params.bungalowId)
+
+	// if (bungalow) res.send(flatted.toJSON(bungalow))
+	if (!bungalow)
+		return res.render('error', {
+			error: { status: 404 },
+			message: `No bungalow found`,
+		})
+
+	loggedInUser.book(bungalow, req.body.checkInDate, req.body.checkOutDate)
+
+	return res.redirect('/bookings')
+})
+
+router.post('/:bungalowId/reviews', (req, res) => {
+	const bungalow = bungalows.find(bung => bung.id === req.params.bungalowId)
+
+	if (!bungalow)
+		return res.render('error', {
+			error: { status: 404 },
+			message: `No bungalow found`,
+		})
+
+	loggedInUser.review(bungalow, req.body.text, req.body.rate)
+
+	return res.redirect(`/bungalows/${bungalow.id}`)
 })
 
 module.exports = router
