d4670584f723000fab38b7387e50f25252cf3496
NSDuman <dumanlife@yandex.com>
2022-09-06 12:45:53 +0300

    Fix book method and add filter conditional

--- src/routes/bungalows.js
+++ src/routes/bungalows.js
@@ -2,6 +2,7 @@ const express = require('express')
 // const flatted = require('flatted')
 const { bungalows } = require('../models')
 const { loggedInUser } = require('../models')
+const { getDays } = require('../models')
 
 const router = express.Router()
 
@@ -10,8 +11,31 @@ router.get('/', (req, res) => {
 	if (req.query.name) {
 		return res.send(bungalows.filter(bungalow => bungalow.name.toLowerCase() === req.query.name.toLowerCase()))
 	}
+
+	if (req.body.checkInDate && req.body.checkOutDate) {
+		const dates = getDays(new Date(req.body.checkInDate), new Date(req.body.checkOutDate))
+
+		const filteredBungalows = bungalows.filter(b => b.bookedDates.some(date => !dates.includes(date)))
+
+		return res.render('bungalows', {
+			title: `Rent a Bungalow for Your Next Escape`,
+			filteredBungalows,
+			user: loggedInUser,
+		})
+	}
 	return res.render('bungalows', { title: `Rent a Bungalow for Your Next Escape`, bungalows, user: loggedInUser })
 })
+
+// router.post('/', (req, res) => {
+// 	const dates = getDays(new Date(req.body.checkInDate), new Date(req.body.checkOutDate))
+// 	const filteredBungalows = bungalows.filter(b => b.bookedDates.some(dates))
+// 	if (!filteredBungalows)
+// 		return res.render('error', {
+// 			error: { status: 404 },
+// 			message: `No bungalow found`,
+// 		})
+// 	return res.redirect('/bungalows')
+// })
 
 router.get('/:bungalowId', (req, res) => {
 	const selectedBungalow = bungalows.find(bungalow => bungalow.id === req.params.bungalowId)
@@ -22,7 +46,7 @@ router.get('/:bungalowId', (req, res) => {
 	else res.sendStatus(404)
 })
 
-router.post('/:bungalowId/bookings', (req, res) => {
+router.post('/:bungalowId', (req, res) => {
 	const bungalow = bungalows.find(bung => bung.id === req.params.bungalowId)
 
 	// if (bungalow) res.send(flatted.toJSON(bungalow))
@@ -31,8 +55,7 @@ router.post('/:bungalowId/bookings', (req, res) => {
 			error: { status: 404 },
 			message: `No bungalow found`,
 		})
-
-	loggedInUser.book(bungalow, req.body.checkInDate, req.body.checkOutDate)
+	loggedInUser.book(bungalow, new Date(req.body.checkInDate), new Date(req.body.checkOutDate))
 
 	return res.redirect('/bookings')
 })
