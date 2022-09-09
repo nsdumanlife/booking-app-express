db45dffea79d1f11e4f7ae13a2c2edc0c47a3611
NSDuman <dumanlife@yandex.com>
2022-09-09 15:38:00 +0300

    Edit loggedinuser

--- src/routes/bookings.js
+++ src/routes/bookings.js
@@ -1,33 +1,20 @@
 const express = require('express')
 // const User = require('../models/user')
-const user = require('../models/index')
+const loggedInUser = require('../models/index')
+// const User = require('../models/user')
 
 const router = express.Router()
 
 /* GET bookings listing. */
 router.get('/', async (req, res, next) => {
 	try {
-		// const user = await User.findById('631a3c3477b43133a0d1db5c')
-
+		// const loggedInUser = await User.findById('631a3c3477b43133a0d1db5c')
+		console.log('LOGGED IN USER: ', loggedInUser)
 		// res.send(flatted.toJSON(numan.bookings))
-		return res.render('bookings', { title: 'Bookings', user, loggedIn: true })
+		return res.render('bookings', { title: 'Bookings', user: loggedInUser, loggedIn: true })
 	} catch (e) {
 		return next(e)
 	}
 })
 
 module.exports = router
-
-// const express = require('express')
-// const { users, loggedInUser } = require('../models')
-
-// const router = express.Router()
-
-// /* GET bookings listing. */
-// router.get('/', (req, res) => {
-// 	const user = users.find(u => u.name === loggedInUser.name)
-// 	// res.send(flatted.toJSON(numan.bookings))
-// 	res.render('bookings', { title: 'Bookings', user: loggedInUser, loggedIn: user === loggedInUser })
-// })
-
-// module.exports = router
