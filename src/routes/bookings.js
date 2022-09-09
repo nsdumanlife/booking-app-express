34e238aa7c455a7cd44f6cdaf9faae498be7a0ca
NSDuman <dumanlife@yandex.com>
2022-09-09 14:21:04 +0300

    Modify cb functions to async and add db methods

--- src/routes/bookings.js
+++ src/routes/bookings.js
@@ -1,13 +1,33 @@
 const express = require('express')
-const { users, loggedInUser } = require('../models')
+// const User = require('../models/user')
+const user = require('../models/index')
 
 const router = express.Router()
 
 /* GET bookings listing. */
-router.get('/', (req, res) => {
-	const user = users.find(u => u.name === loggedInUser.name)
-	// res.send(flatted.toJSON(numan.bookings))
-	res.render('bookings', { title: 'Bookings', user: loggedInUser, loggedIn: user === loggedInUser })
+router.get('/', async (req, res, next) => {
+	try {
+		// const user = await User.findById('631a3c3477b43133a0d1db5c')
+
+		// res.send(flatted.toJSON(numan.bookings))
+		return res.render('bookings', { title: 'Bookings', user, loggedIn: true })
+	} catch (e) {
+		return next(e)
+	}
 })
 
 module.exports = router
+
+// const express = require('express')
+// const { users, loggedInUser } = require('../models')
+
+// const router = express.Router()
+
+// /* GET bookings listing. */
+// router.get('/', (req, res) => {
+// 	const user = users.find(u => u.name === loggedInUser.name)
+// 	// res.send(flatted.toJSON(numan.bookings))
+// 	res.render('bookings', { title: 'Bookings', user: loggedInUser, loggedIn: user === loggedInUser })
+// })
+
+// module.exports = router
